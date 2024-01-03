b8d5c3435d580fcfd78c54d7ee1ef10736d1ccf4
radluki <radluki@gmail.com>
2024-01-03 20:32:05 +0100

    Added ut for getMovies with both arguments set

--- src/services/movie.service.test.ts
+++ src/services/movie.service.test.ts
@@ -25,10 +25,10 @@ it('getMovies when file read returns null', () => {
   expect(movies).toEqual([])
 })
 
-it('getMovies when file read returns movies', () => {
+it('getMovies when arguments not specified', () => {
   jest.spyOn(Math, 'random').mockReturnValue(0.5);
   fileProxyMock.read.mockReturnValueOnce({
-    movies: [{...movie, id: 2}, movie, {...movie, id: 3}]
+    movies: [{ ...movie, id: 2 }, movie, { ...movie, id: 3 }]
   });
   const movies = sut.getMovies();
   expect(movies).toEqual([movie])
@@ -36,10 +36,10 @@ it('getMovies when file read returns movies', () => {
 
 it('getMovies when duration set', () => {
   jest.spyOn(Math, 'random').mockReturnValue(0.9999999);
-  const movie1 = {...movie, id: 1, runtime: 100 };
-  const movie2 = {...movie, id: 2, runtime: 100 };
-  const movie3 = {...movie, id: 3, runtime: 200 };
-  const movie4 = {...movie, id: 4, runtime: 200 };
+  const movie1 = { ...movie, id: 1, runtime: 100 };
+  const movie2 = { ...movie, id: 2, runtime: 100 };
+  const movie3 = { ...movie, id: 3, runtime: 200 };
+  const movie4 = { ...movie, id: 4, runtime: 200 };
   fileProxyMock.read.mockReturnValueOnce({
     movies: [movie1, movie3, movie2, movie4]
   });
@@ -48,15 +48,29 @@ it('getMovies when duration set', () => {
 })
 
 it('getMovies when genres set', () => {
-  jest.spyOn(Math, 'random').mockReturnValue(0.9999999);
-  const movie1 = {...movie, id: 1, genres: ['Action', 'Comedy'] };
-  const movie2 = {...movie, id: 2,  genres: ['Thriller', 'Comedy'] };
-  const movie3 = {...movie, id: 3, genres: ['Action', 'Drama'] };
-  const movie4 = {...movie, id: 4, genres: ['Animation', 'Crime'] };
-  const movie5 = {...movie, id: 5, genres: ['Drama', 'Crime'] };
+  const movie1 = { ...movie, id: 1, genres: ['Action', 'Comedy'] };
+  const movie2 = { ...movie, id: 2, genres: ['Thriller', 'Comedy'] };
+  const movie3 = { ...movie, id: 3, genres: ['Action', 'Drama'] };
+  const movie4 = { ...movie, id: 4, genres: ['Animation', 'Crime'] };
+  const movie5 = { ...movie, id: 5, genres: ['Drama', 'Crime'] };
   fileProxyMock.read.mockReturnValueOnce({
     movies: [movie5, movie3, movie1, movie2, movie4]
   });
   const movies = sut.getMovies(undefined, ['Action', 'Comedy']);
   expect(movies).toEqual([movie1, movie3, movie2])
+})
+
+it('getMovies when genres set and duration set', () => {
+  const movie1 = { ...movie, id: 1, genres: ['Action', 'Comedy'], runtime: 100 };
+  const movie2 = { ...movie, id: 2, genres: ['Thriller', 'Comedy'], runtime: 111 };
+  const movie3 = { ...movie, id: 3, genres: ['Action', 'Drama'], runtime: 110 };
+  const movie4 = { ...movie, id: 4, genres: ['Animation', 'Crime'], runtime: 100 };
+  const movie5 = { ...movie, id: 5, genres: ['Drama', 'Crime'], runtime: 100 };
+  const movie6 = { ...movie, id: 6, genres: ['Action', 'Crime'], runtime: 90 };
+  const movie7 = { ...movie, id: 7, genres: ['Action', 'Crime'], runtime: 89 };
+  fileProxyMock.read.mockReturnValueOnce({
+    movies: [movie5, movie3, movie1, movie2, movie4, movie6, movie7]
+  });
+  const movies = sut.getMovies(100, ['Action', 'Comedy']);
+  expect(movies).toEqual([movie1, movie3, movie6])
 })
