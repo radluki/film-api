14f96908c4a5f4100a95fa42350b0186b2270325
radluki <radluki@gmail.com>
2024-01-03 18:46:23 +0000

    Added testcase for accessing POST /movies when db is removed

--- tests/error-handling.test.ts
+++ tests/error-handling.test.ts
@@ -3,13 +3,26 @@ import { url } from './common';
 import fs from 'fs';
 import { readDbContent, writeDbContent } from './common';
 import { dbPath } from './test-data';
+import { movie } from './test-data';
 
 
-it('when db was removed', async () => {
+it('/movies GET should return 500 when db was removed', async () => {
   const dbdata = readDbContent(dbPath);
 
   fs.rmSync(dbPath);
   const resp = await request(url).get('/movies');
+  writeDbContent(dbPath, dbdata);
+
+  expect(resp.statusCode).toBe(500);
+  expect(resp.body.message).toEqual('Internal Server Error');
+  expect(resp.body.error).toMatch('ENOENT: no such file or directory, open');
+});
+
+it('/movies POST should return 500 when db was removed', async () => {
+  const dbdata = readDbContent(dbPath);
+
+  fs.rmSync(dbPath);
+  const resp = await request(url).post('/movies').send(movie);
   writeDbContent(dbPath, dbdata);
 
   expect(resp.statusCode).toBe(500);
