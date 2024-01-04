76ad5746970534cc7c81e0a238041b9b4546e651
radluki <radluki@gmail.com>
2024-01-04 10:42:25 +0100

    Added config validation

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,36 @@
 import * as dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
+validateConfig();
 
 export const DBPATH: string = process.env.DBPATH;
-export const PORT: number = parseInt(process.env.PORT);
-export const GENRES: string[] = loadGenres();
+export const PORT: number = +process.env.PORT || 3000;
+export const GENRES: string[] = loadGenres(DBPATH);
 
-function loadGenres(): string[] {
-  const { genres } = require(`../${process.env.DBPATH}`)
-  return genres;
+function loadGenres(dbpath): string[] {
+  const { genres } = require(`../${dbpath}`)
+  return genres || [];
+}
+
+function validateConfig() {
+  const { DBPATH, PORT } = process.env;
+  const errors = [];
+  if (PORT && isNaN(+PORT)) {
+    console.warn(`WARNING!!! PORT is set to ${PORT}, using default value 3000`)
+  }
+  if (!DBPATH) {
+    errors.push('DBPATH is not set');
+  }
+  if (!fs.existsSync(DBPATH)) {
+    errors.push(`DBPATH ${DBPATH} file does not exist`);
+  }
+  if (errors.length > 0) {
+    console.error(`ERROR!!! Invalid configuration, reasons:`, errors);
+    process.exit(1);
+  }
+  const GENRES = loadGenres(DBPATH);
+  if (GENRES.length == 0) {
+    console.warn('WARNING!!! No genres found in DBPATH file')
+  }
 }
