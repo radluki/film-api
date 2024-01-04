fdb8d77e79225662c1d7335fb1d47e003e11dea2
radluki <radluki@gmail.com>
2024-01-04 11:22:57 +0100

    movies.router.ts extracted to a file

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
-import express, { Router } from 'express';
+import express from 'express';
 import { IMovieService, MovieService } from './services/movie.service';
 import { FileProxy } from './utils/file-proxy';
 import bodyParser from 'body-parser';
 import { DBPATH, PORT } from './config';
-import { validateMoviesGetQuery } from './middleware/movies-get-query.validation';
-import { validateMoviesPostBody } from './middleware/movies-post-body.validation';
-import { createMoviesGetController } from './controllers/movies-get.controller';
-import { createMoviesPostController } from './controllers/movies-post.controller';
 import { errorHandler } from './middleware/error-handler';
 import { NumericConversionsFileProxyDecorator } from './utils/file-proxy-decorator';
+import { createMoviesRouter } from './routers/movies.router';
 
 
 const fileProxy = new FileProxy(DBPATH);
 const fileProxyWithNumericConversions = new NumericConversionsFileProxyDecorator(fileProxy);
 const movieService: IMovieService = new MovieService(fileProxyWithNumericConversions);
 
-const moviesGetController = createMoviesGetController(movieService);
-const moviesPostController = createMoviesPostController(movieService);
-
-const moviesRouter = Router();
-moviesRouter.get('/', validateMoviesGetQuery, moviesGetController);
-moviesRouter.post('/', validateMoviesPostBody, moviesPostController);
-
 const app = express();
 app.use(bodyParser.json());
-app.use('/movies', moviesRouter);
-app.use(errorHandler); // Should be the last to overwite the default error handler
+app.use('/movies', createMoviesRouter(movieService));
+app.use(errorHandler); // Should be the last app.use() call
 app.listen(PORT, () => console.log(`Movie Server is running on http://localhost:${PORT}`));
---
+++ src/routers/movies.router.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { IMovieService } from '../services/movie.service';
+import { validateMoviesGetQuery } from '../middleware/movies-get-query.validation';
+import { validateMoviesPostBody } from '../middleware/movies-post-body.validation';
+import { createMoviesGetController } from '../controllers/movies-get.controller';
+import { createMoviesPostController } from '../controllers/movies-post.controller';
+
+export function createMoviesRouter(movieService: IMovieService) {
+  const moviesRouter = Router();
+
+  const moviesGetController = createMoviesGetController(movieService);
+  const moviesPostController = createMoviesPostController(movieService);
+
+  moviesRouter.get('/', validateMoviesGetQuery, moviesGetController);
+  moviesRouter.post('/', validateMoviesPostBody, moviesPostController);
+
+  return moviesRouter;
+}
