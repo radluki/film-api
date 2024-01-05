98b02a30cf939c28f9468f575561a3f3c27a57ee
radluki <radluki@gmail.com>
2024-01-05 10:57:29 +0100

    config.ts migrated to use joi

--- src/config.ts
+++ src/config.ts
@@ -2,46 +2,53 @@ import * as dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
 import { dbDataSchema } from "./utils/db-validation";
+import Joi from "joi";
 
 dotenv.config();
-validateConfig();
 
-export const { DBPATH } = process.env;
-export const PORT: number = +process.env.PORT || 3000;
-export const GENRES: string[] = loadGenres(DBPATH);
+const validatePathExists = (value) => {
+  if (!fs.existsSync(value)) {
+    throw new Error(`${value} file does not exist`);
+  }
+  return value;
+};
+
+const envVarsSchema = Joi.object({
+  PORT: Joi.number().default(3000),
+  DBPATH: Joi.string().required().custom(validatePathExists),
+}).unknown();
+
+function validateEnvVars(envVars): EnvVars {
+  const { error, value } = envVarsSchema.validate(envVars);
+  if (error) {
+    console.error(`Config validation error: ${error.message}`);
+    process.exit(1);
+  }
+  return value;
+}
+
+const envVars = validateEnvVars(process.env);
+export const { DBPATH, PORT }: EnvVars = envVars;
+export const GENRES = loadGenres(DBPATH);
+
+interface EnvVars {
+  PORT: number;
+  DBPATH: string;
+}
 
 function loadGenres(dbpath): string[] {
   dbpath = path.resolve(dbpath);
   console.log(`Loading genres from ${dbpath}`);
   const rawData = JSON.parse(fs.readFileSync(dbpath, "utf-8"));
-
   const {
     error,
     value: { genres },
   } = dbDataSchema.validate(rawData);
   if (error) {
-    console.error(`ERROR!!! Database validation failed:`, error.message);
+    console.error(`Database validation failed: ${error.message}`);
     process.exit(1);
   }
 
   console.log(`Loaded genres:`, genres);
   return genres;
-}
-
-function validateConfig() {
-  const { DBPATH, PORT } = process.env;
-  const errors = [];
-  if (PORT && isNaN(+PORT)) {
-    console.warn(`WARNING!!! PORT is set to ${PORT}, using default value 3000`);
-  }
-  if (!DBPATH) {
-    errors.push("DBPATH is not set");
-  }
-  if (!fs.existsSync(DBPATH)) {
-    errors.push(`DBPATH ${DBPATH} file does not exist`);
-  }
-  if (errors.length > 0) {
-    console.error(`ERROR!!! Invalid configuration, reasons:`, errors);
-    process.exit(1);
-  }
 }
