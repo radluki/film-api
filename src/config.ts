569c806b49940fa68e16af10d3c18366472efd22
radluki <radluki@gmail.com>
2024-01-05 14:05:45 +0000

    GENRES reloading

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,26 @@ import {
   loadGenres,
   validateEnvVars,
 } from "./utils/config-validation";
+import fs from "fs";
+import { logger } from "./utils/logger";
 
 dotenv.config();
 
 export const { DBPATH, PORT }: EnvVars = validateEnvVars(process.env);
-export const GENRES = loadGenres(DBPATH);
+export let GENRES = loadGenres(DBPATH);
+
+const debounce = (func, delay) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+};
+
+
+fs.watch(DBPATH, debounce((eventType) => {
+  if (eventType === 'change') {
+    logger.info(`${DBPATH} has changed. Reloading...`);
+    GENRES = loadGenres(DBPATH);
+  }
+}, 200));
