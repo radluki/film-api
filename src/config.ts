d979c224748e7ed8cf0c6092aab9bc37efe6d7b2
radluki <radluki@gmail.com>
2024-01-05 11:02:36 +0100

    Extracted config-validation.ts

--- src/config.ts
+++ src/config.ts
@@ -1,54 +1,11 @@
 import * as dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
-import { dbDataSchema } from "./utils/db-validation";
-import Joi from "joi";
+import {
+  EnvVars,
+  loadGenres,
+  validateEnvVars,
+} from "./utils/config-validation";
 
 dotenv.config();
 
-const validatePathExists = (value) => {
-  if (!fs.existsSync(value)) {
-    throw new Error(`${value} file does not exist`);
-  }
-  return value;
-};
-
-const envVarsSchema = Joi.object({
-  PORT: Joi.number().default(3000),
-  DBPATH: Joi.string().required().custom(validatePathExists),
-}).unknown();
-
-function validateEnvVars(envVars): EnvVars {
-  const { error, value } = envVarsSchema.validate(envVars);
-  if (error) {
-    console.error(`Config validation error: ${error.message}`);
-    process.exit(1);
-  }
-  return value;
-}
-
-const envVars = validateEnvVars(process.env);
-export const { DBPATH, PORT }: EnvVars = envVars;
+export const { DBPATH, PORT }: EnvVars = validateEnvVars(process.env);
 export const GENRES = loadGenres(DBPATH);
-
-interface EnvVars {
-  PORT: number;
-  DBPATH: string;
-}
-
-function loadGenres(dbpath): string[] {
-  dbpath = path.resolve(dbpath);
-  console.log(`Loading genres from ${dbpath}`);
-  const rawData = JSON.parse(fs.readFileSync(dbpath, "utf-8"));
-  const {
-    error,
-    value: { genres },
-  } = dbDataSchema.validate(rawData);
-  if (error) {
-    console.error(`Database validation failed: ${error.message}`);
-    process.exit(1);
-  }
-
-  console.log(`Loaded genres:`, genres);
-  return genres;
-}
---
+++ src/utils/config-validation.ts
@@ -0,0 +1,50 @@
+import * as dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import { dbDataSchema } from "./db-validation";
+import Joi from "joi";
+
+dotenv.config();
+
+const validatePathExists = (value) => {
+  if (!fs.existsSync(value)) {
+    throw new Error(`${value} file does not exist`);
+  }
+  return value;
+};
+
+const envVarsSchema = Joi.object({
+  PORT: Joi.number().default(3000),
+  DBPATH: Joi.string().required().custom(validatePathExists),
+}).unknown();
+
+export function validateEnvVars(envVars): EnvVars {
+  const { error, value } = envVarsSchema.validate(envVars);
+  if (error) {
+    console.error(`Config validation error: ${error.message}`);
+    process.exit(1);
+  }
+  return value;
+}
+
+export interface EnvVars {
+  PORT: number;
+  DBPATH: string;
+}
+
+export function loadGenres(dbpath): string[] {
+  dbpath = path.resolve(dbpath);
+  console.log(`Loading genres from ${dbpath}`);
+  const rawData = JSON.parse(fs.readFileSync(dbpath, "utf-8"));
+  const {
+    error,
+    value: { genres },
+  } = dbDataSchema.validate(rawData);
+  if (error) {
+    console.error(`Database validation failed: ${error.message}`);
+    process.exit(1);
+  }
+
+  console.log(`Loaded genres:`, genres);
+  return genres;
+}
