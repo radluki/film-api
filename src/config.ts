944ccaf9227fd0383af81b59986fd735e2c7394b
radluki <radluki@gmail.com>
2024-01-07 11:16:09 +0000

    GENRES array encapsulated in genres module

--- src/config.ts
+++ src/config.ts
@@ -1,31 +1,9 @@
 import * as dotenv from "dotenv";
 import {
   EnvVars,
-  loadGenres,
   validateEnvVars,
 } from "./utils/config-validation";
-import fs from "fs";
-import { logger } from "./utils/logger";
 
 dotenv.config();
 
 export const { DBPATH, PORT }: EnvVars = validateEnvVars(process.env);
-export let GENRES = loadGenres(DBPATH);
-
-const debounce = (func, delay) => {
-  let timeout;
-  return (...args) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), delay);
-  };
-};
-
-fs.watch(
-  DBPATH,
-  debounce((eventType) => {
-    if (eventType === "change") {
-      logger.info(`${DBPATH} has changed. Reloading...`);
-      GENRES = loadGenres(DBPATH);
-    }
-  }, 200),
-);
--- src/utils/genres.ts
+++ src/utils/genres.ts
@@ -1,6 +1,29 @@
-import { GENRES } from "../config";
+import fs from "fs";
+import { logger } from "./logger";
+import { loadGenres } from "./config-validation";
+import { DBPATH } from "../config";
 
 
 export function isGenreValid(genre) {
   return GENRES.includes(genre);
 }
+
+let GENRES = loadGenres(DBPATH);
+
+const debounce = (func, delay) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+};
+
+fs.watch(
+  DBPATH,
+  debounce((eventType) => {
+    if (eventType === "change") {
+      logger.info(`${DBPATH} has changed. Reloading...`);
+      GENRES = loadGenres(DBPATH);
+    }
+  }, 200),
+);
