05afcbee93dd88c8a6610a1eecd7d6a10359a488
radluki <radluki@gmail.com>
2024-01-07 16:20:55 +0100

    Mocking createMoviesPostController in movies.router.post.test.ts

--- src/routers/movies.router.post.test.ts
+++ src/routers/movies.router.post.test.ts
@@ -1,13 +1,10 @@
 import express from "express";
 import { createMoviesRouter } from "./movies.router";
-import { IMovieService, MovieCreationResult } from "../services/movie.service";
+import { IMovieService } from "../services/movie.service";
 import request from "supertest";
 import { Movie } from "../models/db.types";
 import { validateMoviesPostBody as validate } from "../middleware/movies-post-body.validation";
-
-const movieServiceMock = {
-  createMovie: jest.fn(),
-};
+import { createMoviesPostController } from "../controllers/movies-post.controller";
 
 const GENRE1 = "genre1";
 const GENRE2 = "genre2";
@@ -24,83 +21,76 @@ jest.mock("../utils/genres", () => ({}));
 jest.mock("../middleware/movies-post-body.validation", () => ({
   validateMoviesPostBody: jest.fn(),
 }));
+jest.mock("../controllers/movies-post.controller", () => ({
+  createMoviesPostController: jest.fn(),
+}));
+(createMoviesPostController as any).mockImplementation(() => controllerMock);
+
 const validateMock = validate as unknown as jest.Mock;
+const controllerMock = jest.fn();
+const movieServiceMock = {};
 
 const app = express();
 app.use("/", createMoviesRouter(movieServiceMock as unknown as IMovieService));
 
 beforeEach(() => {
   jest.clearAllMocks();
-  validateMock.mockImplementation((req, res, next) => next());
 });
 
-// TODO split POST tests in similar way it was done for GET
 describe("movies router POST /", () => {
   const id = 44;
   const message = "Movie created - fake";
   const msg = { id, message };
   const CREATE = 201;
-  const successResult = new MovieCreationResult(CREATE, msg);
 
-  it("should call service with body", () => {
-    movieServiceMock.createMovie.mockReturnValueOnce(successResult);
-    return request(app)
-      .post("/")
-      .send(movie)
-      .expect(CREATE)
-      .expect((res) => {
-        expect(res.body).toEqual(msg);
-        expect(movieServiceMock.createMovie).toHaveBeenCalledWith(movie);
-      });
-  });
-
-  it("should forward errors from service", () => {
-    const error = "error - fake";
-    const msg = { error };
-    const status = 409;
-    const errorResult = new MovieCreationResult(status, msg);
-    movieServiceMock.createMovie.mockReturnValueOnce(errorResult);
-    return request(app)
-      .post("/")
-      .send(movie)
-      .expect(status)
-      .expect((res) => {
-        expect(res.body).toEqual(msg);
-        expect(movieServiceMock.createMovie).toHaveBeenCalledWith(movie);
-      });
-  });
-
-  it('should not call service when "validate" fails', () => {
-    validateMock.mockReset();
-    const errors = ["validation failed"];
-    validateMock.mockImplementation((req, res) =>
-      res.status(400).json({ errors }),
+  it("should pass validate and return controller response", () => {
+    validateMock.mockImplementation((req, res, next) => next());
+    controllerMock.mockImplementation((req, res) =>
+      res.status(CREATE).json(msg),
     );
     return request(app)
       .post("/")
       .send(movie)
-      .expect(400)
-      .expect({ errors })
+      .expect(CREATE)
+      .expect(msg)
       .expect(() => {
-        expect(movieServiceMock.createMovie).not.toHaveBeenCalled();
+        expect(validateMock).toHaveBeenCalledTimes(1);
       });
   });
 
-  it("post / should return 500 when service throws", () => {
-    movieServiceMock.createMovie.mockReset();
-    movieServiceMock.createMovie.mockImplementation(() => {
-      throw new Error("XXX");
+  it('should not call controller when "validate" fails', () => {
+    const errors = ["validation failed"];
+    validateMock.mockImplementation((req, res) =>
+      res.status(400).json({ errors }),
+    );
+    return request(app).post("/").send(movie).expect(400).expect({ errors });
+  });
+
+  it("post / should return 500 when validate throws", () => {
+    validateMock.mockImplementation(() => {
+      throw new Error("validate throw");
+    });
+    return request(app).post("/").send(movie).expect(500).expect({
+      error: "validate throw",
+      message: "Internal Server Error",
+    });
+  });
+
+  it("post / should return 500 when controller throws", () => {
+    validateMock.mockImplementation((req, res, next) => next());
+    controllerMock.mockImplementation(() => {
+      throw new Error("XXXtre");
     });
     return request(app)
       .post("/")
       .send(movie)
       .expect(500)
-      .expect((res) => {
-        expect(res.body).toEqual({
-          error: "XXX",
-          message: "Internal Server Error",
-        });
-        expect(movieServiceMock.createMovie).toHaveBeenCalledWith(movie);
+      .expect({
+        error: "XXXtre",
+        message: "Internal Server Error",
+      })
+      .expect(() => {
+        expect(validateMock).toHaveBeenCalledTimes(1);
       });
   });
 });
